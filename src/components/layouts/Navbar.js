89982fd460248851a6542561e0b4b95d654731af
sharaseaman <shara.seaman@gmail.com>
2020-03-25 14:17:56 -0500

    add title and icon props and proptypes

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+
 
 export class Navbar extends Component {
+    static defaultProps = {
+        title: 'Home Inventory',
+        icon: 'fas fa-home px-3'
+    }
+
+    static propTypes= {
+        title: PropTypes.string.isRequired,
+        icon: PropTypes.string.isRequired,
+    }
+    
     render() {
         return (
             <nav className="navbar bg-primary">
                 <h1>
-                    <i className="fas fa-home" />
-                    
+                    <i className={this.props.icon} /> {this.props.title}
                 </h1>
             </nav>
         )
