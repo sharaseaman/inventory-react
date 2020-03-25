fe0c57ad1f470fca0d9a4a6d5676e2f03a232b37
sharaseaman <shara.seaman@gmail.com>
2020-03-25 10:59:02 -0500

    refactor funtional based component to class based component for learning purposes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Shara says Hello World</h1>
-      <p class="text-xl-left">You can use the mark tag to <mark>highlight</mark> text.</p>
-    </div>
-  );
+//FUNCTIONAL COMPONENT
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Shara says Hello World</h1>
+//       You can use the mark tag to <mark>highlight</mark> text.
+//     </div>
+//   );
+// }
+
+//CHANGING TO CLASS BASED COMPONENT
+//* replace funtion with 'class' and remove () after App
+//* cant return directly from a class, we need a method render()
+//* render is a life cycle methhod that runs at a certain point, it is required
+//* extend the core react component class with React.Component
+//can use class App extends React.component or
+// remove the React from React.component and add { component } to the react import
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>Shara says Hello World</h1>
+          <p className="text-uppercase">Bootstrap is working, all uppercase</p>
+      </div>
+    )
+  }
 }
 
 export default App;
