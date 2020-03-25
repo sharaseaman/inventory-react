d1070ba599ad7a81cc816e064f0007bd65b3d910
sharaseaman <shara.seaman@gmail.com>
2020-03-25 12:43:16 -0500

    add font awesome and navbar component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Navbar from './components/layouts/Navbar.js';
 import './App.css';
 
 //FUNCTIONAL COMPONENT
@@ -58,6 +59,7 @@ class App extends Component {
   render(){
     return (
       <div className="App">
+        < Navbar />
         <h1>Shara says Hello World</h1>
           <p className="text-uppercase">Bootstrap is working, all uppercase</p>
       </div>
---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar bg-primary">
+                <h1>
+                    <i className="fas fa-home" />
+                    
+                </h1>
+            </nav>
+        )
+    }
+}
+
+export default Navbar
