151c074b33061e71e2bf435b24c3b22a1991396d
sharaseaman <shara.seaman@gmail.com>
2020-03-25 11:31:00 -0500

    notes about Fragments and JSX

--- src/App.js
+++ src/App.js
@@ -19,6 +19,41 @@ import './App.css';
 //can use class App extends React.component or
 // remove the React from React.component and add { component } to the react import
 
+// JSX NOTES:
+// *everything must be wrapped in only one parent element
+// *to remove the parent elelment you can use <React.Fragment></React.Fragment> 
+    // expample: 
+    //    <div className="App">
+    //       <h1>Shara says Hello World</h1>
+    //       You can use the mark tag to <mark>highlight</mark> text.
+    //     </div>
+    // replace the div with React.Fragment to remove the div elelment 
+    //     <React.Fragment>
+    //       <h1>Shara says Hello World</h1>
+    //       You can use the mark tag to <mark>highlight</mark> text.
+    //     </React.Fragment>
+    // to refact that even more, you can remove React and import { Fragment }
+    ///     <Fragment>
+    //       <h1>Shara says Hello World</h1>
+    //       You can use the mark tag to <mark>highlight</mark> text.
+    //     <Fragment>
+    // import React, { Fragment }{ Component } from 'react';
+// * empty <> will also work instead of <Fragment></Fragment>
+
+// JSX refactor to javascript 
+//  return (
+//  <div className="App">
+//    <h1>Shara says Hello World</h1>
+//    <p className="text-uppercase">Bootstrap is working, all uppercase</p>
+//  </div> 
+//  )
+// to: 
+// return React.createElement( 
+//  'div', 
+//  {classname: 'App" }, 
+//  React.createElement('h1, null, 'Hello from React')
+//  );
+
 class App extends Component {
   render(){
     return (
